5e8b099ef3081ab2be9a4e8067855e1ec0022463
jemish-169 <jemishkhunt169@gmail.com>
2022-09-27 22:21:22 +0530

    sign in/up UI complete

--- app/src/main/java/com/ssip/buzztalk/ui/fragments/auth/ChooseLoginSignUpFragment.kt
+++ app/src/main/java/com/ssip/buzztalk/ui/fragments/auth/ChooseLoginSignUpFragment.kt
@@ -1,14 +1,13 @@
 package com.ssip.buzztalk.ui.fragments.auth
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.ssip.buzztalk.R
 import com.ssip.buzztalk.databinding.FragmentChooseLoginSignUpBinding
-import com.ssip.buzztalk.databinding.FragmentLoginBinding
 
 class ChooseLoginSignUpFragment : Fragment() {
 
@@ -22,7 +21,7 @@ class ChooseLoginSignUpFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentChooseLoginSignUpBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -30,11 +29,11 @@ class ChooseLoginSignUpFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.signUp.setOnClickListener {
+        binding.btnSignup.setOnClickListener {
             findNavController().navigate(R.id.action_chooseLoginSignUpFragment_to_signUpFragment)
         }
 
-        binding.login.setOnClickListener {
+        binding.btnLogin.setOnClickListener {
             findNavController().navigate(R.id.action_chooseLoginSignUpFragment_to_loginFragment)
         }
     }
