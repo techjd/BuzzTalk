37a003eda79e02739be97cd112580807535fe698
techjd <jaydeepparmar253@gmail.com>
2022-09-30 00:19:43 +0530

    Added Reactions Library , Fixed Bug of Infinite Loop of BackStack, Removed Dark theme

--- app/src/main/java/com/ssip/buzztalk/utils/Constants.kt
+++ app/src/main/java/com/ssip/buzztalk/utils/Constants.kt
@@ -1,7 +1,7 @@
 package com.ssip.buzztalk.utils
 
 object Constants {
-    const val DEVELOPMENT_BASE_URL = "http://192.168.2.4:5500/api/"
+    const val DEVELOPMENT_BASE_URL = "http://192.168.2.5:5500/api/"
     const val PRODUCTION_BASE_URL = "" // Enter Deployed Server URL
     const val PREFS_TOKEN_FILE = "PREFS_TOKEN_FILE"
     const val USER_TOKEN = "USER_TOKEN"
